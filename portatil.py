bacc2b810fd81fd6da02b5a11f9c138ead52c761
mmili24 <mmilicevic@institutmvm.cat>
2026-01-30 12:22:26 +0100

    Test portatil OK, codi actualitzat

--- portatil.py
+++ portatil.py
@@ -1,10 +1,10 @@
 from producte import Producte
 
 class Portatil(Producte):
-    def __init__(self, ram, cpu):
+    def __init__(self, nom, preu, stock, ram, cpu):
         super().__init__(nom, preu, stock)
         self.ram = ram
-        self.cpu = cpu
+        self.processador = cpu
 
     def __str__(self):
         return f"PORTÀTIL:{self.nom} | ramGB {self.ram} | {self.cpu}"
